856f8bbfcececb535a213f975780b865e48a6d9a
Bianca Correa <me@aquabianca.dev>
2026-05-23 19:32:51 -0300

    feat: default to communist.mom domain and save links with port

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	addr := "0.0.0.0:45800"
 	log.Printf("Starting Mercury Shortener Service with Fiber...")
 	log.Printf("Dashboard accessible at http://%s", addr)
-	log.Printf("Shortlink format: http://mercury.<domain>:45800/<key>")
+	log.Printf("Shortlink format: http://mercury.<domain>/<key> (e.g., http://mercury.communist.mom/<key>)")
 
 	if err := srv.Start(addr); err != nil {
 		log.Fatalf("Server stopped: %v", err)
